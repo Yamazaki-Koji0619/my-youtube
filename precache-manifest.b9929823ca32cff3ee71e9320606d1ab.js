4cf89d9e443a015ab2d645135f5568fb5f791e57
Yamazaki-Koji0619 <kouzi619soccer@icloud.com>
2020-11-22 16:32:29 +0900

    Updates

---
+++ precache-manifest.b9929823ca32cff3ee71e9320606d1ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41f954bda2912b02afe28c3cb28dbc4",
+    "url": "/my-youtube/index.html"
+  },
+  {
+    "revision": "2120e80b028be60b020c",
+    "url": "/my-youtube/static/css/main.c5d663e4.chunk.css"
+  },
+  {
+    "revision": "84244a42453f5abfbdbf",
+    "url": "/my-youtube/static/js/2.1487e4ad.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-youtube/static/js/2.1487e4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2120e80b028be60b020c",
+    "url": "/my-youtube/static/js/main.92432e08.chunk.js"
+  },
+  {
+    "revision": "bc47cc47671c5d693a5f",
+    "url": "/my-youtube/static/js/runtime-main.3406b144.js"
+  }
+]);
